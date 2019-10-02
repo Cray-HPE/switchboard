e748fa2459ae3d80f7a0053bbb930fee37e5a823
Alan Mutschelknaus <alanm@cray.com>
2019-10-01 21:39:27 -0500

    Save time

--- switchboard.sh
+++ switchboard.sh
@@ -1,29 +1,35 @@
 #!/bin/bash
 
+# Set format to json so jq can be used
+export CRAY_FORMAT=json
+
 function uas_output_parse() {
   echo $1 | grep $2| grep -o '".*' | tr -d '"'
 }
 
-export CRAY_FORMAT=json
+function create_uai() {
+  UAS_CREATE=$(cray uas create --publickey ~/.ssh/id_rsa.pub)
+}
+
+# Automatically generate rsa keys if they don't exist.
+# These keys are required to create and log in to UAIs
+function verify_ssh_keys_exist() {
+  yes no | ssh-keygen -f ~/.ssh/id_rsa -N "" > /dev/null
+}
 
 echo "Authenticate with Cray DC LDAP..."
-cray init --overwrite --no-auth \ 
+cray init --overwrite --no-auth \
           --hostname https://api-gw-service-nmn.local > /dev/null
 
-# Check if a simple command returns 401. If it doesn't
+# Check if a simple command returns 401. If it doesn't,
 # we don't need to force the user to authenticate again 
 # as they may already have a valid token
 if cray uas mgr-info list 2>&1 | grep --silent "401 Unauthorized"; then
   cray auth login --username $USER
 fi
 
-# Automatically generate rsa keys if they don't exist.
-# These keys are required to create and log in to
-# User Access Instances
-yes no | ssh-keygen -f ~/.ssh/id_rsa -N "" > /dev/null
-
-echo "Checking for running UAI..."
-UAS_LIST=$(cray uas list --format json)
+echo "Checking for running UAIs..."
+UAS_LIST=$(cray uas list)
 
 # Make sure we are able to parse the UAS_LIST with jq
 if ! echo $UAS_LIST | jq -e > /dev/null; then
@@ -31,10 +37,26 @@ if ! echo $UAS_LIST | jq -e > /dev/null; then
   exit 1
 fi
 
-NUM_UAI=$(echo $UAS_LIST | jq '.|length')
-echo "Found $NUM_UAI instances:"
-echo $UAS_LIST | jq -r '.[] | .uai_name'
+# TODO use this nifty trick
+# jq '.[] | select(.uai_status=="Running: Ready")'
 
-if [ $NUM_UAI -gt 0 ]; then
+NUM_UAI=$(echo $UAS_LIST | jq '.|length')
+
+if [ $NUM_UAI -lt 1 ]; then
+  echo "Creating a UAI..."
+  create_uai
+  exit 0
+fi
+
+if [ $NUM_UAI -eq 1 ]; then
+  echo "Using existing UAI connection string..."
   $(echo $UAS_LIST | jq -r '.[0] | .uai_connect_string')
+  exit 0
+fi
+
+if [ $NUM_UAI -gt 1 ]; then
+  echo $UAS_LIST | jq -r '.[] | "\(.uai_name) \t \(.uai_status) \t \(.uai_img)"' | awk '{print NR, "\t", $0}'
+  read -p "Select a UAI by number: " selection
+  $(echo $UAS_LIST | jq -r --arg INDEX $selection '.[$INDEX|tonumber] | .uai_connect_string')
+  exit 0
 fi
