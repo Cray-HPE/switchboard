36f0828680f5697531f8a8afa58124d587f0bdfe
Alan Mutschelknaus <alanm@cray.com>
2020-02-20 14:56:59 -0600

    CASMUAS-56: Cleanup unused code and formatting

--- cmd/root.go
+++ cmd/root.go
@@ -5,11 +5,8 @@ import (
 	"os"
 	"os/exec"
 
-	homedir "github.com/mitchellh/go-homedir"
-	"github.com/spf13/viper"
 	"github.com/spf13/cobra"
 )
-var cfgFile string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -27,46 +24,7 @@ switchboard delete)`,
 }
 
 func init() {
-	cobra.OnInitialize(initConfig)
 	checkCmdExists("cray")
-
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.switchboard.yaml)")
-
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	//rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-}
-
-
-// initConfig reads in config file and ENV variables if set.
-func initConfig() {
-	if cfgFile != "" {
-		// Use config file from the flag.
-		viper.SetConfigFile(cfgFile)
-	} else {
-		// Find home directory.
-		home, err := homedir.Dir()
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-
-		// Search config in home directory with name ".switchboard" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigName(".switchboard")
-	}
-
-	viper.AutomaticEnv() // read in environment variables that match
-
-	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Println("Using config file:", viper.ConfigFileUsed())
-	}
 }
 
 // Check for craycli
@@ -77,14 +35,6 @@ func checkCmdExists(cmd string) {
 		os.Exit(1)
 	}
 }
-
-// Check that SSH keypairs exist
-//func checkCmdExists(cmd string) {
-//}
-
-// Check Cray CLI is authenticated
-//func checkCmdExists() {
-//}
 
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
--- cmd/uai/uai.go
+++ cmd/uai/uai.go
@@ -1,12 +1,12 @@
 package uai
 
 import (
-        "fmt"
-        "log"
-        "os"
-        "os/exec"
-        "text/tabwriter"
-        "encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"text/tabwriter"
+	"encoding/json"
 
 	homedir "github.com/mitchellh/go-homedir"
 )
@@ -14,17 +14,17 @@ import (
 /* 
 A struct to represent a UAI from uas-mgr which has a json representation of
 [
-  {
-    "uai_age": "4d17h",
-    "uai_connect_string": "ssh alanm@172.30.48.15 -p 31370 -i ~/.ssh/id_rsa",
-    "uai_host": "ncn-w001",
-    "uai_img": "bis.local:5000/cray/cray-uas-sles15-slurm:latest",
-    "uai_msg": "ContainerCreating",
-    "uai_name": "uai-alanm-b1a72874",
-    "uai_portmap": {},
-    "uai_status": "Waiting",
-    "username": "alanm"
-  },
+	{
+		"uai_age": "4d17h",
+		"uai_connect_string": "ssh alanm@172.30.48.15 -p 31370 -i ~/.ssh/id_rsa",
+		"uai_host": "ncn-w001",
+		"uai_img": "bis.local:5000/cray/cray-uas-sles15-slurm:latest",
+		"uai_msg": "ContainerCreating",
+		"uai_name": "uai-alanm-b1a72874",
+		"uai_portmap": {},
+		"uai_status": "Waiting",
+		"username": "alanm"
+	},
 ]
 */
 type Uai struct {
@@ -47,42 +47,42 @@ func UaiCreate() Uai {
 	}
 	cmd := exec.Command("cray", "uas", "create", "--format", "json",
 			 "--publickey", home+"/.ssh/id_rsa.pub")
-        var uai Uai
-        stdout, err := cmd.StdoutPipe()
-        if err != nil {
-                log.Fatal(err)
-        }
+	var uai Uai
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Println("Creating a new UAI...")
-        if err := cmd.Start(); err != nil {
-                log.Fatal(err)
-        }
-        err = json.NewDecoder(stdout).Decode(&uai)
-        if (err != nil) {
-                log.Fatal(err)
-        }
-        if err := cmd.Wait(); err != nil {
-                log.Fatal(err)
-        }
+	if err := cmd.Start(); err != nil {
+		log.Fatal(err)
+	}
+	err = json.NewDecoder(stdout).Decode(&uai)
+	if (err != nil) {
+		log.Fatal(err)
+	}
+	if err := cmd.Wait(); err != nil {
+		log.Fatal(err)
+	}
 	return uai
 }
 
 // Run cray uas list and decode the json into a slice of type Uai
 func UaiList() []Uai {
 	cmd := exec.Command("cray", "uas", "list", "--format", "json")
-        var uais []Uai
-        stdout, err := cmd.StdoutPipe()
-        if err != nil {
-                log.Fatal(err)
-        }
-        if err := cmd.Start(); err != nil {
-                log.Fatal(err)
-        }
-        if err := json.NewDecoder(stdout).Decode(&uais); err != nil {
-                log.Fatal(err)
-        }
-        if err := cmd.Wait(); err != nil {
-                log.Fatal(err)
-        }
+	var uais []Uai
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := cmd.Start(); err != nil {
+		log.Fatal(err)
+	}
+	if err := json.NewDecoder(stdout).Decode(&uais); err != nil {
+		log.Fatal(err)
+	}
+	if err := cmd.Wait(); err != nil {
+		log.Fatal(err)
+	}
 	return uais
 }
 
@@ -90,20 +90,20 @@ func UaiList() []Uai {
 func UaiDelete(uais string) {
 	cmd := exec.Command("cray", "uas", "delete", "--format", "json",
 			 "--uai-list", uais)
-        _, err := cmd.StdoutPipe()
-        if err != nil {
-                log.Fatal(err)
-        }
+	_, err := cmd.StdoutPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Println("Deleting UAI(s)...")
-        if err := cmd.Start(); err != nil {
-                log.Fatal(err)
-        }
-        if (err != nil) {
-                log.Fatal(err)
-        }
-        if err := cmd.Wait(); err != nil {
-                log.Fatal(err)
-        }
+	if err := cmd.Start(); err != nil {
+		log.Fatal(err)
+	}
+	if (err != nil) {
+		log.Fatal(err)
+	}
+	if err := cmd.Wait(); err != nil {
+		log.Fatal(err)
+	}
 }
 
 /*
@@ -113,15 +113,15 @@ Func to tabwriter a slice of type Uai in the format:
 2       uai-alanm-f6a0e079      Running:Ready                   19m     bis.local:5000/cray/cray-uas-sles15-slurm:latest
 */
 func UaiPrettyPrint(uais []Uai) {
-        w := new(tabwriter.Writer)
-        w.Init(os.Stdout, 0, 8, 0, '\t', 0)
-        if len(uais) > 0 {
-                fmt.Fprintln(w, "#\tName\tStatus\tAge\tImage")
-        }
-        for i,u := range uais {
-                fmt.Fprintf(w, "%d\t%s\t%s%s\t%s\t%s\n", i+1, u.Name,
-                                u.StatusMessage, u.Status,
-                                u.Age, u.Image)
-        }
-        w.Flush()
+	w := new(tabwriter.Writer)
+	w.Init(os.Stdout, 0, 8, 0, '\t', 0)
+	if len(uais) > 0 {
+		fmt.Fprintln(w, "#\tName\tStatus\tAge\tImage")
+	}
+	for i,u := range uais {
+		fmt.Fprintf(w, "%d\t%s\t%s%s\t%s\t%s\n", i+1, u.Name,
+				u.StatusMessage, u.Status,
+				u.Age, u.Image)
+	}
+	w.Flush()
 }
